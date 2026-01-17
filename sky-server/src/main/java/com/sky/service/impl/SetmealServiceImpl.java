740cff702cff6334d9c93a73da5f87cac77b91fc
modestian <acworld2004@163.com>
2026-01-17 11:30:47 +0800

    day04 新增套餐分页查询业务相关代码开发

--- sky-server/src/main/java/com/sky/service/impl/SetmealServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/SetmealServiceImpl.java
@@ -1,12 +1,18 @@
 package com.sky.service.impl;
 
+import com.github.pagehelper.Page;
+import com.github.pagehelper.PageHelper;
 import com.sky.dto.SetmealDTO;
+import com.sky.dto.SetmealPageQueryDTO;
 import com.sky.entity.Dish;
 import com.sky.entity.Setmeal;
 import com.sky.entity.SetmealDish;
 import com.sky.mapper.SetmealDishMapper;
 import com.sky.mapper.SetmealMapper;
+import com.sky.result.PageResult;
 import com.sky.service.SetmealService;
+import com.sky.vo.DishVO;
+import com.sky.vo.SetmealVO;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -49,7 +55,22 @@ public class SetmealServiceImpl implements SetmealService {
 
             setmealDishMapper.insertBatch(setmealDishes);
         }
+    }
 
+    /**
+     * 套餐分页查询
+     * @param setmealPageQueryDTO
+     * @return
+     */
+    public PageResult pageQuery(SetmealPageQueryDTO setmealPageQueryDTO){
+        //开始分页查询(用mybatis中的pageHelper插件实现)
+        PageHelper.startPage(setmealPageQueryDTO.getPage(), setmealPageQueryDTO.getPageSize());
+        //前后看似没有关系，但是由于pagehelper的部分参数是源于threadlocal，而每一次请求thread都是同一个，所以是可以向后传参的
+        Page<SetmealVO> page = setmealMapper.pageQuery(setmealPageQueryDTO);
 
+        long total = page.getTotal();
+        List<SetmealVO> records = page.getResult();
+
+        return new PageResult(total,records);
     }
 }
