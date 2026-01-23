78e75039753f52e20a5a754dfb39fdb05e66126d
modestian <acworld2004@163.com>
2026-01-23 16:28:28 +0800

    day10 新增用户数量统计(包括新增用户与总用户数量)功能代码开发

--- sky-server/src/main/java/com/sky/service/impl/ReportServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/ReportServiceImpl.java
@@ -3,10 +3,13 @@ package com.sky.service.impl;
 import com.sky.entity.Orders;
 import com.sky.mapper.DishMapper;
 import com.sky.mapper.OrderMapper;
+import com.sky.mapper.UserMapper;
 import com.sky.service.ReportService;
 import com.sky.vo.TurnoverReportVO;
+import com.sky.vo.UserReportVO;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
+import org.apache.poi.ss.formula.functions.T;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -25,6 +28,11 @@ public class ReportServiceImpl implements ReportService {
     @Autowired
     private OrderMapper orderMapper;
 
+    @Autowired
+    private UserMapper userMapper;
+    @Autowired
+    private ReportService reportService;
+
     /**
      * 统计指定时间区间内的营业额
      * @param begin
@@ -33,14 +41,8 @@ public class ReportServiceImpl implements ReportService {
      */
     public TurnoverReportVO getTurnOverStatistics(LocalDate begin, LocalDate end){
 
-        List<LocalDate> dateList = new ArrayList();//用于存放begin到end每一天的日期
+        List<LocalDate> dateList = getDateListBetween(begin, end);//用于存放begin到end每一天的日期
         List<Double> turnoverList = new ArrayList<>();
-        dateList.add(begin);
-
-        while(!begin.equals(end)){
-            begin = begin.plusDays(1);
-            dateList.add(begin);
-        }
 
         for(LocalDate date:dateList){
             LocalDateTime beginTime = LocalDateTime.of(date, LocalTime.MIN);
@@ -60,5 +62,67 @@ public class ReportServiceImpl implements ReportService {
                 .dateList(StringUtils.join(dateList, ","))
                 .turnoverList(StringUtils.join(turnoverList, ","))
                 .build();
+    }
+
+    /**
+     * 统计指定时间区间内的用户数据
+     * @param begin
+     * @param end
+     * @return
+     */
+    public UserReportVO getUserStatistics(LocalDate begin, LocalDate end){
+        //存档begin到end之前每天的日期
+        List<LocalDate> dateList = getDateListBetween(begin, end);
+
+        //存放每天新增用户数量
+        List<Integer> newUserList = new ArrayList<>();
+
+        //存放每天的总用户数量
+        List<Integer> totalUserList = new ArrayList<>();
+
+        for(LocalDate date:dateList){
+            LocalDateTime beginTime = LocalDateTime.of(date, LocalTime.MIN);
+            LocalDateTime endTime = LocalDateTime.of(date, LocalTime.MAX);
+
+            Map map = new HashMap();
+
+            map.put("endTime", endTime);
+            Integer totalUser = userMapper.countByMap(map);
+
+            map.put("beginTime", beginTime);
+            Integer newUser = userMapper.countByMap(map);
+
+            newUserList.add(newUser);
+            totalUserList.add(totalUser);
+        }
+
+        //封装数据结果
+        return UserReportVO
+                .builder()
+                .dateList(StringUtils.join(dateList,","))
+                .newUserList(StringUtils.join(newUserList,","))
+                .totalUserList(StringUtils.join(totalUserList,","))
+                .build();
+    }
+
+    /**
+     * 根据起始和结束日期得到日期列表 private
+     * @param begin
+     * @param end
+     * @return
+     */
+    private List<LocalDate> getDateListBetween(LocalDate begin, LocalDate end){
+        //初始化
+        List<LocalDate> dateList = new ArrayList<>();
+
+        //加入首个日期
+        dateList.add(begin);
+
+        //加入剩下的日期
+        while(!begin.equals(end)){
+            begin = begin.plusDays(1);
+            dateList.add(begin);
+        }
+        return dateList;
     }
 }
