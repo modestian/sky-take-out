6c0f4a613017bbd1f56009b57a596891d13f9013
modestian <acworld2004@163.com>
2026-01-23 09:59:47 +0800

    day10 新增订单状态自动处理业务功能代码开发

--- sky-server/src/main/java/com/sky/controller/admin/OrderController.java
+++ sky-server/src/main/java/com/sky/controller/admin/OrderController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController("adminOrderController")
 @RequestMapping("/admin/order")
-@Api("订单管理接口")
+@Api(tags = "订单管理接口")
 @Slf4j
 public class OrderController {
 
---
+++ sky-server/src/main/java/com/sky/task/OrderTask.java
@@ -0,0 +1,58 @@
+package com.sky.task;
+
+import com.sky.entity.Orders;
+import com.sky.mapper.OrderMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Component
+@Slf4j
+public class OrderTask {
+
+    @Autowired
+    private OrderMapper orderMapper;
+
+    /**
+     * 处理超时订单
+     */
+    @Scheduled(cron = "0 * * * * ?")
+    public void processTimeOutOrder() {
+        log.info("定时处理超时订单:{}", LocalDateTime.now());
+
+        List<Orders> ordersList = orderMapper.getByStatusAndOrderTimeLT(Orders.PENDING_PAYMENT, LocalDateTime.now().plusMinutes(-15));
+
+        if(ordersList != null && ordersList.size() > 0) {
+            for(Orders order : ordersList) {
+                order.setStatus(Orders.CANCELLED);
+                order.setCancelReason("订单超时，自动取消。");
+                order.setCancelTime(LocalDateTime.now());
+                orderMapper.update(order);
+            }
+        }
+
+    }
+
+    /**
+     * 每天凌晨1点处理“派送中”订单
+     */
+    @Scheduled(cron = "0 0 1 * * ?")
+    //@Scheduled(cron = "0/5 * * * * *")
+    public void processDeliveryOrder() {
+        log.info("处理一直处于“派送中的订单:{}", LocalDateTime.now());
+
+        List<Orders> ordersList = orderMapper.getByStatusAndOrderTimeLT(Orders.DELIVERY_IN_PROGRESS, LocalDateTime.now().plusHours(-1));
+
+        if(ordersList != null && ordersList.size() > 0) {
+            for(Orders order : ordersList) {
+                order.setStatus(Orders.COMPLETED);
+                orderMapper.update(order);
+            }
+        }
+    }
+
+}
